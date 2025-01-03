99b731157b823e2757a6f8ecc6a70faf819de8cc
KGJR02 <reangeloa02@gmail.com>
2025-01-04 00:49:17 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.use(express.static("public")); // Serve static files
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
