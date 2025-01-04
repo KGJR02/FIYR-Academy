cb403e792a5d3265776402cdd8f4aadb8bc4c520
KGJR02 <reangeloa02@gmail.com>
2025-01-04 02:47:32 +0100

    Update scripts.js

--- public/scripts.js
+++ public/scripts.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("FIYR Academy is live!");
 });
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -13,7 +13,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBsioqqEHDnneX4ULeevST6mO1NYWxhh-I",
   authDomain: "fiyr-academy.firebaseapp.com",
   projectId: "fiyr-academy",
-  storageBucket: "fiyr-academy.firebasestorage.app",
+  storageBucket: "fiyr-academy.appspot.com",
   messagingSenderId: "1082192041153",
   appId: "1:1082192041153:web:9de39c9799fc173e2f7040",
   measurementId: "G-GQF03FYF45"
@@ -22,3 +22,26 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+// Fetch Blogs
+async function fetchBlogs() {
+    const blogsCollection = collection(db, "blogs");
+    const snapshot = await getDocs(blogsCollection);
+    snapshot.forEach((doc) => {
+        console.log(doc.id, "=>", doc.data());
+    });
+}
+
+// Fetch Courses
+async function fetchCourses() {
+    const coursesCollection = collection(db, "courses");
+    const snapshot = await getDocs(coursesCollection);
+    snapshot.forEach((doc) => {
+        console.log(doc.id, "=>", doc.data());
+    });
+}
+
+// Call the functions
+fetchBlogs();
+fetchCourses();
