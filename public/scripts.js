a3bb99bb58c5d9d9b86a30dd7d2645f4e7e59b70
KGJR02 <reangeloa02@gmail.com>
2025-01-04 02:34:00 +0100

    Update scripts.js

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("FIYR Academy is live!");
 });
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBsioqqEHDnneX4ULeevST6mO1NYWxhh-I",
+  authDomain: "fiyr-academy.firebaseapp.com",
+  projectId: "fiyr-academy",
+  storageBucket: "fiyr-academy.firebasestorage.app",
+  messagingSenderId: "1082192041153",
+  appId: "1:1082192041153:web:9de39c9799fc173e2f7040",
+  measurementId: "G-GQF03FYF45"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
