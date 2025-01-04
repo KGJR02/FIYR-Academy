500fe779ebbf9809db8f8bcc1c84a436898af738
KGJR02 <reangeloa02@gmail.com>
2025-01-04 01:01:34 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,3 +4,6 @@ const PORT = 3000;
 
 app.use(express.static("public")); // Serve static files
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
